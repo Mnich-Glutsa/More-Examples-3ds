ab076da563405fc996b1e9b191890de45314bad4
Glutsa <glutsa@glutsa.ovh>
2025-11-01 00:03:33 +0100

    found when error is

--- Playing-Wav-3ds/source/main.c
+++ Playing-Wav-3ds/source/main.c
@@ -19,11 +19,11 @@ int main(int argc, char **argv)
 
     consoleInit(GFX_TOP, NULL);
 
-    printf("1;1HCheckpoint 1");
+    printf("\x1b[1;1HCheckpoint 1");
 
     ncsndInit(true);
 
-    printf("2;1HCheckpoint 2");
+    printf("\x1b[2;1HCheckpoint 2");
 
     CWAV* cwav = (CWAV*)malloc(sizeof(CWAV));
     FILE* file = fopen("romfs:/hiscore.bcwav", "rb");
@@ -31,23 +31,27 @@ int main(int argc, char **argv)
     u32 filesize = ftell(file);
     void* buffer = linearAlloc(filesize);
 
-    printf("3;1HCheckpoint 3");
+    printf("\x1b[3;1HCheckpoint 3");
 
     if (!buffer) svcBreak(USERBREAK_PANIC);
 
     fseek(file, 0, SEEK_SET);
-    fread(buffer, 1, filesize, file);
-    fclose(file);                           //w tym bloku się coś pierdoli
+    printf("\x1b[4;1HCheckpoint 4");
+    fread(buffer, 1, filesize, file); //o tu dokładnie się psuje
+    printf("\x1b[5;1HCheckpoint 5");
+    fclose(file);
+    printf("\x1b[6;1HCheckpoint 6");
     cwavLoad(cwav, buffer, 1);
+    printf("\x1b[7;1HCheckpoint 7");
     cwav->dataBuffer = buffer;
 
-    printf("4;1HCheckpoint 4");
+    printf("\x1b[8;1HCheckpoint 8");
 
-    printf("15;1HA - play");
-    printf("16;1HSTART - exitt");
+    printf("\x1b[15;1HA - play");
+    printf("\x1b[16;1HSTART - exitt");
     while (aptMainLoop()) {
         if (hidKeysDown() & KEY_START) break; //Click start to exit program
-        if (hidKeysDown() & KEY_A) { cwavPlay(cwav, 0, 1); printf("3;1HSound!"); }
+        if (hidKeysDown() & KEY_A) { cwavPlay(cwav, 0, 1); printf("\x1b[18;1HSound!"); }
     }
 
 	gfxExit();
